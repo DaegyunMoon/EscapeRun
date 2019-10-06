be72d8ed21105ee6086cd1c3dee41a5451d3386c
gujinzzang <tnwls3607@gmail.com>
2019-10-06 22:35:16 +0900

    캐릭터 상태 추가 아이템 획득 개수 표시

---
+++ HideAndSeek/Assets/Invector-3rdPersonController/Basic Locomotion LITE/Scripts/CharacterController/Editor/vCharacterState.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Invector;
+
+public class vCharacterState : MonoBehaviour
+{
+    public static vCharacterState instance;
+
+    private int numOfGetItem;
+    public int NumOfGetItem
+    {
+        get
+        {
+            return numOfGetItem;
+        }
+        set
+        {
+            numOfGetItem = value;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numOfGetItem = 0;
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
