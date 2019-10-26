5505e024faac6787a569a6c584a1bce85ff68731
DaegyunMoon <moondg1019@gmail.com>
2019-10-27 03:54:56 +0900

    캐릭터 상태 구현 피로도 / 잠수 / 탈진 / 추락높이 등

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.UI;
+using System;
 
 public class PlayerControl : MonoBehaviour {
-    private enum PlayerState { Idle, Run, Sprint, Jump, Fall, Exhaustion, Dive, Death }
+    public enum PlayerState { Idle, Run, Sprint, Jump, Fall, Exhaust, Dive, Death }
+    [SerializeField] private float hp = 100;
     [SerializeField] private float moveSpeed = 3;
     [SerializeField] private float turnSpeed = 200;
-    [SerializeField] private float jumpForce = 6;
+    [SerializeField] private float jumpForce = 12;
     [SerializeField] private Animator animator;
     [SerializeField] private Rigidbody rigidBody;
 
-    PlayerState playerState;
+    public PlayerState playerState;
     private float currentV = 0;
     private float currentH = 0;
 
@@ -20,15 +23,20 @@ public class PlayerControl : MonoBehaviour {
 
     private bool isGrounded;
     private bool wasGrounded;
+    private bool isJumping;
 
     private float jumpTimeStamp = 0;
     private float minJumpInterval = 0.25f;
+    public float maximumHeight = 0f;
     private List<Collider> collisions = new List<Collider>();
+
+    public Text hptext;
 
     void Update()
     {
         animator.SetBool("Grounded", isGrounded);
-        TankUpdate();
+        MoveUpdate();
+        CheckState();
         wasGrounded = isGrounded;
     }
 
@@ -91,12 +99,46 @@ public class PlayerControl : MonoBehaviour {
             isGrounded = false;
         }
     }
-    private void TankUpdate()
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Water") && playerState != PlayerState.Exhaust)
+        {
+            playerState = PlayerState.Dive;
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Water") && playerState != PlayerState.Exhaust)
+        {
+            playerState = PlayerState.Dive;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Water") && playerState != PlayerState.Exhaust)
+        {
+            playerState = PlayerState.Fall;
+        }
+    }
+    private void MoveUpdate()
     {
         float v = Input.GetAxis("Vertical");
         float h = Input.GetAxis("Horizontal");
 
-        bool sprint = Input.GetKey(KeyCode.LeftShift);
+        bool sprint = Input.GetKey(KeyCode.LeftShift) && playerState != PlayerState.Exhaust;
+        bool CanChangeState()
+        {
+            switch(playerState)
+            {
+                case PlayerState.Jump:
+                case PlayerState.Dive:
+                case PlayerState.Exhaust:
+                    return false;
+                default:
+                    return true;
+            }
+        }
 
         if (v < 0)
         {
@@ -109,9 +151,32 @@ public class PlayerControl : MonoBehaviour {
                 v *= backwardWalkScale;
             }
         }
-        else if (sprint)
+        else if (sprint & Mathf.Abs(v) >= 0.1f)
         {
             v *= runScale;
+            hp -= 0.2f;
+        }
+        if (playerState == PlayerState.Dive || playerState == PlayerState.Exhaust)
+        {
+            v *= 0.5f;
+        }
+        if (isGrounded && CanChangeState())
+        {
+            if (Mathf.Abs(v) < 0.1)
+            {
+                playerState = PlayerState.Idle;
+            }
+            else
+            {
+                if (sprint)
+                {
+                    playerState = PlayerState.Sprint;
+                }
+                else
+                {
+                    playerState = PlayerState.Run;
+                }
+            }
         }
 
         currentV = Mathf.Lerp(currentV, v, Time.deltaTime * interpolation);
@@ -131,23 +196,106 @@ public class PlayerControl : MonoBehaviour {
 
         if (jumpCooldownOver && isGrounded && Input.GetKey(KeyCode.Space))
         {
+            if(playerState == PlayerState.Exhaust)
+            {
+                jumpForce = 6f;
+            }
+            else
+            {
+                jumpForce = 12f;
+            }
             jumpTimeStamp = Time.time;
             rigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            playerState = PlayerState.Jump;
+            isJumping = true;
         }
-
         if (!wasGrounded && isGrounded)
         {
-            animator.SetTrigger("Land");
+            float fallAmount = maximumHeight - this.transform.position.y;
+            Debug.Log("Fall Amount : " + fallAmount);
+            if(fallAmount > 0.7f)
+            {
+                animator.SetTrigger("Land");
+            }
+            maximumHeight = this.transform.position.y;
+            if(hp < 0.0f)
+            {
+                playerState = PlayerState.Exhaust;
+            }
         }
-
         if (!isGrounded && wasGrounded)
         {
             animator.SetTrigger("Jump");
+            if(isJumping)
+            {
+                playerState = PlayerState.Jump;
+            }
+            else
+            {
+                playerState = PlayerState.Fall;
+            }
         }
     }
 
     private void CheckState()
     {
+        switch(playerState)
+        {
+            case PlayerState.Idle:
+                maximumHeight = this.transform.position.y;
+                if(hp < 100.0f)
+                {
+                    hp += 0.5f;
+                }
+                break;
+            case PlayerState.Run:
+                maximumHeight = this.transform.position.y;
+                isJumping = false;
+                break;
+            case PlayerState.Sprint:
+                maximumHeight = this.transform.position.y;
+                isJumping = false;
+                break;
+            case PlayerState.Jump:
+                isJumping = true;
+                if (maximumHeight <= this.transform.position.y)
+                {
+                    maximumHeight = this.transform.position.y;
+                }
+                else
+                {
+                    playerState = PlayerState.Fall;
+                    isJumping = false;
+                }
+                break;
+            case PlayerState.Fall:
+                break;
+            case PlayerState.Exhaust:
+                maximumHeight = this.transform.position.y;
+                break;
+            case PlayerState.Dive:
+                maximumHeight = this.transform.position.y;
+                break;
+            case PlayerState.Death:
+                break;
+        }
 
+        hptext.text = Mathf.Round(hp).ToString();
+        if(hp > 100.0f)
+        {
+            hp = 100.0f;
+        }
+        if(hp <= 0f && hp > -100f && isGrounded)
+        {
+            hp = -100f;
+            playerState = PlayerState.Exhaust;
+            Invoke("Recover", 10.0f);
+        }
+    }
+
+    private void Recover()
+    {
+        hp = 0.1f;
+        playerState = PlayerState.Idle;
     }
 }
