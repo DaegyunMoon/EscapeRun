4c15807a4dd7f5f1845f737e52874d33b8741d26
SyuniCho <jscho9605@gmail.com>
2019-10-29 21:50:35 +0900

    add timer

---
+++ Assets/Timer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public static float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+     
+        int t = Mathf.FloorToInt(time);
+        Text uiText = GetComponent<Text>();
+        uiText.text = "Time: " + t;
+    }
+}
